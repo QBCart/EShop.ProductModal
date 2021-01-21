5186ec15681e33177a2a74bc2f8fd777d50d3af6
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-01-21 10:14:32 -0500

    fixed index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,2 @@
-import React from "react";
-import { render } from "react-dom";
 import  ProductModal from './product-modal';
-
-const mountModal = ( addToCartFunction, productModalId?: string ) => {
-  render(<ProductModal triggerId={productModalId} addToCart ={addToCartFunction} />, document.getElementById('product-modal'));
-};
-
-export default mountModal;
+export default ProductModal;
