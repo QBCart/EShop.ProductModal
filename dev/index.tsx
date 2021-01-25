a12a148e629fec742d4774bbfbec9ab8e80f869f
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-01-25 09:19:45 -0500

    Specify index

--- dev/index.tsx
+++ dev/index.tsx
@@ -3,7 +3,7 @@ import React, {
 } from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js';
 import { render } from 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js';
 
-import ProductModal from '../src';
+import ProductModal from '../src/index';
 
 interface Props {}
 
