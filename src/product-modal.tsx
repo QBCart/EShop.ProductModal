a21b6d3922457ffdbf86b0118c8444fb484e5843
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-02-02 10:28:25 -0500

    toast has been built

--- src/product-modal.tsx
+++ src/product-modal.tsx
@@ -268,31 +268,24 @@ const ProductModal: FC<Props> = (props) => {
         </div>
       </div>
       {/* toast */}
-      <div
-        aria-live="polite"
-        aria-atomic="true"
-        style={{ position: 'relative', minHeight: '200px' }}
-      >
+      <div aria-live="polite" aria-atomic="true">
         <div
           className="toast"
-          style={{ position: 'absolute', top: 0, right: 0 }}
+          data-delay="800"
+          style={{ position: 'absolute', top: '10%', right: '10%' }}
         >
           <div className="toast-header">
-            <img src="..." className="rounded mr-2" alt="..."></img>
-            <strong className="mr-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
             <button
               type="button"
               className="ml-2 mb-1 close"
               data-dismiss="toast"
-              data-delay="1000"
               aria-label="Close"
             >
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div className="toast-body">
-            Hello, world! This is a toast message.
+            Item has been successfully added to your cart!
           </div>
         </div>
       </div>
