514e9d173b19e27d3de4d5627b85f229aaee404d
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-02-19 14:13:14 -0500

    removed item check from top level
    Co-authored-by: Allan Mobley <allan.mobley.jr@outlook.com>

--- src/product-modal.tsx
+++ src/product-modal.tsx
@@ -93,224 +93,211 @@ const ProductModal: FC<Props> = (props) => {
         aria-hidden="true"
       >
         <div className="modal-dialog modal-dialog-scrollable modal-xl modal-lg">
-          {item ? (
-            <div className="modal-content">
-              <div className="modal-body">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-12 col-lg-4">
-                      {/* begin Carousel Section */}
-                      <div
-                        id="carouselExampleIndicators"
-                        className="carousel slide"
-                        data-ride="carousel"
-                        data-interval="false"
-                      >
-                        {item.Images && item.Images.length > 0 ? (
-                          <ol className="carousel-indicators">
-                            <li
-                              data-target="#carouselExampleIndicators"
-                              data-slide-to="0"
-                              className="active"
-                            ></li>
-                            {item.Images.map((img, index) => {
-                              return (
-                                <li
-                                  key={`${item.id}-carousel-indicator-${index}`}
-                                  data-target="#carouselExampleIndicators"
-                                  data-slide-to={index + 1}
-                                ></li>
-                              );
-                            })}
-                          </ol>
-                        ) : null}
-                        <div className="carousel-inner">
-                          {item.id ? (
-                            <div
-                              className="carousel-item active d-flex align-items-center justify-content-center"
-                              // style={{
-                              //   backgroundImage: `url(${props.companyStorageUrl}images/responsive/${item.id})`
-                              // }}
-                            >
-                              <img
-                                className="img-fluid"
-                                src={`${props.companyStorageUrl}images/responsive/${item.id}`}
-                              />
-                            </div>
-                          ) : (
-                            <div className="carousel-item active"></div>
-                          )}
-                          {item.Images && item.Images.length > 0
-                            ? item.Images.map((img, index) => {
-                                return (
-                                  <div
-                                    key={`${item.id}-carousel-img-${index}`}
-                                    className="carousel-item"
-                                    style={{
-                                      backgroundImage: `url(${props.companyStorageUrl}images/related/${item.id}/responsive/${img})`
-                                    }}
-                                  ></div>
-                                );
-                              })
-                            : null}
+          <div className="modal-content">
+            <div className="modal-body">
+              <div className="container">
+                <div className="row">
+                  <div className="col-12 col-lg-4">
+                    {/* begin Carousel Section */}
+                    <div
+                      id="carouselExampleIndicators"
+                      className="carousel slide"
+                      data-ride="carousel"
+                      data-interval="false"
+                    >
+                      {item?.Images && item.Images.length > 0 ? (
+                        <ol className="carousel-indicators">
+                          <li
+                            data-target="#carouselExampleIndicators"
+                            data-slide-to="0"
+                            className="active"
+                          ></li>
+                          {item.Images.map((img, index) => {
+                            return (
+                              <li
+                                key={`${item.id}-carousel-indicator-${index}`}
+                                data-target="#carouselExampleIndicators"
+                                data-slide-to={index + 1}
+                              ></li>
+                            );
+                          })}
+                        </ol>
+                      ) : null}
+                      <div className="carousel-inner">
+                        <div className="carousel-item active d-flex align-items-center justify-content-center">
+                          <img
+                            className="img-fluid"
+                            src={
+                              item?.id
+                                ? `${props.companyStorageUrl}images/responsive/${item.id}`
+                                : `${props.companyStorageUrl}images/white-space.jpg`
+                            }
+                          />
                         </div>
-                        <a
-                          className="carousel-control-prev"
-                          id="carousel-prev"
-                          href="#carouselExampleIndicators"
-                          role="button"
-                          data-slide="prev"
-                        >
-                          <span
-                            className="carousel-control-prev-icon"
-                            aria-hidden="true"
-                          ></span>
-                          <span className="sr-only">Previous</span>
-                        </a>
-                        <a
-                          className="carousel-control-next"
-                          id="carousel-prev"
-                          href="#carouselExampleIndicators"
-                          role="button"
-                          data-slide="next"
-                        >
-                          <span
-                            className="carousel-control-next-icon"
-                            aria-hidden="true"
-                          ></span>
-                          <span className="sr-only">Next</span>
-                        </a>
+                        {item?.Images && item.Images.length > 0
+                          ? item.Images.map((img, index) => {
+                              return (
+                                <div
+                                  key={`${item.id}-carousel-img-${index}`}
+                                  className="carousel-item"
+                                  style={{
+                                    backgroundImage: `url(${props.companyStorageUrl}images/related/${item.id}/responsive/${img})`
+                                  }}
+                                ></div>
+                              );
+                            })
+                          : null}
                       </div>
-                      {/* begin Add Space Section */}
-                      <div className="row ad-space"></div>
-                    </div>
-                    {/* begin Scroll Box Section */}
-                    <div className="col-12 col-lg-8">
-                      <ul
-                        className="nav-pill-scroll-box-header nav nav-pills mb-3"
-                        id="pills-tab"
-                        role="tablist"
+                      <a
+                        className="carousel-control-prev"
+                        id="carousel-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="prev"
                       >
-                        <li className="nav-item" role="presentation">
-                          <a
-                            className="nav-link active"
-                            id="pills-overview-tab"
-                            data-toggle="pill"
-                            href="#pills-overview"
-                            role="tab"
-                            aria-controls="pills-overview"
-                            aria-selected="true"
-                          >
-                            Overview
-                          </a>
-                        </li>
-                        <li className="nav-item" role="presentation">
-                          <a
-                            className="nav-link .scroll-box-tabs"
-                            id="pills-specs-tab"
-                            data-toggle="pill"
-                            href="#pills-specs"
-                            role="tab"
-                            aria-controls="pills-specs"
-                            aria-selected="false"
-                          >
-                            Specs
-                          </a>
-                        </li>
-                        <li className="nav-item" role="presentation">
-                          <a
-                            className="nav-link .scroll-box-tabs"
-                            href={item.Href}
-                            role="tab"
-                            aria-controls="pills-specs"
-                            aria-selected="false"
-                          >
-                            Visit Page
-                          </a>
-                        </li>
-                      </ul>
-                      <div className="nav-pill-scroll-box">
-                        <div className="tab-content" id="pills-tabContent">
-                          <div
-                            className="tab-pane fade show active"
-                            id="pills-overview"
-                            role="tabpanel"
-                            aria-labelledby="pills-overview-tab"
-                          >
-                            <h3>Product ID: {item.Name}</h3>
-                            <h4>Description: {item.SalesDesc}</h4>
-                            <h4>Price: {toUSCurrency(item.SalesPrice)}</h4>
-                            <h4>Details:</h4>
-                            <p>{item.FullDesc}</p>
-                          </div>
-                          <div
-                            className="tab-pane fade"
-                            id="pills-specs"
-                            role="tabpanel"
-                            aria-labelledby="pills-specs-tab"
-                          >
-                            {item.Specs && item.Specs.length > 0 ? (
-                              item.Specs.map((textline, index) => {
-                                return (
-                                  <div
-                                    key={`${item.id}-specs-textline-${index}`}
-                                  >
-                                    {textline}
-                                  </div>
-                                );
-                              })
-                            ) : (
-                              <div>
-                                <h3>Sorry, we have no specs!</h3>
-                                <div>
-                                  The specs for this item are not yet available.
+                        <span
+                          className="carousel-control-prev-icon"
+                          aria-hidden="true"
+                        ></span>
+                        <span className="sr-only">Previous</span>
+                      </a>
+                      <a
+                        className="carousel-control-next"
+                        id="carousel-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="next"
+                      >
+                        <span
+                          className="carousel-control-next-icon"
+                          aria-hidden="true"
+                        ></span>
+                        <span className="sr-only">Next</span>
+                      </a>
+                    </div>
+                    {/* begin Add Space Section */}
+                    <div className="row ad-space"></div>
+                  </div>
+                  {/* begin Scroll Box Section */}
+                  <div className="col-12 col-lg-8">
+                    <ul
+                      className="nav-pill-scroll-box-header nav nav-pills mb-3"
+                      id="pills-tab"
+                      role="tablist"
+                    >
+                      <li className="nav-item" role="presentation">
+                        <a
+                          className="nav-link active"
+                          id="pills-overview-tab"
+                          data-toggle="pill"
+                          href="#pills-overview"
+                          role="tab"
+                          aria-controls="pills-overview"
+                          aria-selected="true"
+                        >
+                          Overview
+                        </a>
+                      </li>
+                      <li className="nav-item" role="presentation">
+                        <a
+                          className="nav-link .scroll-box-tabs"
+                          id="pills-specs-tab"
+                          data-toggle="pill"
+                          href="#pills-specs"
+                          role="tab"
+                          aria-controls="pills-specs"
+                          aria-selected="false"
+                        >
+                          Specs
+                        </a>
+                      </li>
+                      <li className="nav-item" role="presentation">
+                        <a
+                          className="nav-link .scroll-box-tabs"
+                          href={item?.Href}
+                          role="tab"
+                          aria-controls="pills-specs"
+                          aria-selected="false"
+                        >
+                          Visit Page
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="nav-pill-scroll-box">
+                      <div className="tab-content" id="pills-tabContent">
+                        <div
+                          className="tab-pane fade show active"
+                          id="pills-overview"
+                          role="tabpanel"
+                          aria-labelledby="pills-overview-tab"
+                        >
+                          <h3>Product ID: {item?.Name}</h3>
+                          <h4>Description: {item?.SalesDesc}</h4>
+                          <h4>Price: {toUSCurrency(item?.SalesPrice || 0)}</h4>
+                          <h4>Details:</h4>
+                          <p>{item?.FullDesc}</p>
+                        </div>
+                        <div
+                          className="tab-pane fade"
+                          id="pills-specs"
+                          role="tabpanel"
+                          aria-labelledby="pills-specs-tab"
+                        >
+                          {item?.Specs && item.Specs.length > 0 ? (
+                            item.Specs.map((textline, index) => {
+                              return (
+                                <div key={`${item.id}-specs-textline-${index}`}>
+                                  {textline}
                                 </div>
+                              );
+                            })
+                          ) : (
+                            <div>
+                              <h3>Sorry, we have no specs!</h3>
+                              <div>
+                                The specs for this item are not yet available.
                               </div>
-                            )}
-                          </div>
+                            </div>
+                          )}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              {/* begin Footer Section */}
-              <div className="modal-footer">
-                <div className="col-6 d-flex justify-content-start">
-                  <input
-                    type="number"
-                    step="1"
-                    min="1"
-                    onChange={setQuantity}
-                    value={item.Quantity}
-                    className="form-control-lg ml-2 mr-1"
-                  ></input>
-                  <button
-                    onClick={() => {
-                      submitToCart();
-                    }}
-                    type="button"
-                    className="btn btn-success"
-                  >
-                    <span className="material-icons">add_shopping_cart</span>
-                  </button>
-                </div>
-                <div className="col-6 d-flex justify-content-end">
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    data-dismiss="modal"
-                  >
-                    <span className="material-icons">close</span>
-                  </button>
-                </div>
+            </div>
+            {/* begin Footer Section */}
+            <div className="modal-footer">
+              <div className="col-6 d-flex justify-content-start">
+                <input
+                  type="number"
+                  step="1"
+                  min="1"
+                  onChange={setQuantity}
+                  value={item?.Quantity}
+                  className="form-control-lg ml-2 mr-1"
+                ></input>
+                <button
+                  onClick={() => {
+                    submitToCart();
+                  }}
+                  type="button"
+                  className="btn btn-success"
+                >
+                  <span className="material-icons">add_shopping_cart</span>
+                </button>
+              </div>
+              <div className="col-6 d-flex justify-content-end">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-dismiss="modal"
+                >
+                  <span className="material-icons">close</span>
+                </button>
               </div>
             </div>
-          ) : (
-            <div className="modal-content">
-              <div className="modal-body"></div>
-            </div>
-          )}
+          </div>
         </div>
       </div>
       <AddToCartToast companyStorageUrl={props.companyStorageUrl} />
