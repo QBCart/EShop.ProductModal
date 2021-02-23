70f40bae4b6d79f49eb1bb0321601557bd389d9e
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-02-23 10:33:18 -0500

    Exported interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import ProductModal from './product-modal';
-import ProductModalItem from './types/product-modal-item';
+import ProductModalItem from "./types/product-modal-item";
 
-export { ProductModal, ProductModalItem };
+export {ProductModal, ProductModalItem} ;
