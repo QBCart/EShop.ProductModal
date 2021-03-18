633ca2dacdc53bf447dc6306acbf35d022caa453
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-18 09:52:28 -0400

    updated dev

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,13 +1,2 @@
-import * as React from './skypack';
-import { render } from './skypack';
-
-import AddToCartTest from './addToCartTest';
-
-render(
-  <AddToCartTest
-    imagesStorageUrl={
-      'https://qbcimages4e2rfby2glfmo.z13.web.core.windows.net/'
-    }
-  />,
-  document.getElementById('cart')
-);
+import mountProductModal from '../src/index';
+mountProductModal();
