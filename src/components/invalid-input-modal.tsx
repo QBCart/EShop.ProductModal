f0426d2177d7e4fcb99b5dbe7c13b636b10c5ce3
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-18 08:39:01 -0400

    added copy

--- src/components/invalid-input-modal.tsx
+++ src/components/invalid-input-modal.tsx
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ * This code can only be used and/or distributed with express permission.
+ */
+
 import * as React from '../skypack';
 import type { FC } from '../skypack';
 
