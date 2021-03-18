a44458da887b1e56693a57540c68cd4f10d29790
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-18 08:38:18 -0400

    added mounting code

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,24 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ * This code can only be used and/or distributed with express permission.
+ */
+
+import * as React from './skypack';
+import { render } from './skypack';
 import ProductModal from './product-modal';
-export default ProductModal;
+
+const mountProductModal = () => {
+  const id = 'qbc-eshop-product-modal';
+  const mountingDiv = document.getElementById(id);
+
+  render(
+    <ProductModal
+      id={id}
+      imagesStorageUrl={mountingDiv.dataset.imagesStorageUrl}
+    />,
+    mountingDiv
+  );
+};
+
+export default mountProductModal;
