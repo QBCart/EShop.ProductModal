91039d453b5ff0f0ff8e8b46afb5885dd99fc6c6
Michael Shea <mshea424@yahoo.com>
2021-03-30 13:36:59 -0400

    update
    Co-authored-by: Allan Mobley <allan-mobley-jr@users.noreply.github.com>

--- snowpack.demo.config.js
+++ snowpack.demo.config.js
@@ -21,6 +21,7 @@ module.exports = {
     '@qbcart/eshop-local-db': 'https://qbcdemo.z13.web.core.windows.net/eshop/localdb/index.js',
     '@qbcart/utils': 'https://qbcdemo.z13.web.core.windows.net/utils/index.js',
     '@qbcart/toast': 'https://qbcdemo.z13.web.core.windows.net/toast/index.js',
+    '@qbcart/alert-modal': 'https://qbcdemo.z13.web.core.windows.net/alert-modal/index.js',
     'product-modal': 'https://qbcdemo.z13.web.core.windows.net/eshop/product-modal/index.js'
   }
 };
