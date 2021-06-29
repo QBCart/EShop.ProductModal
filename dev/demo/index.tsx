6c1792853d4a1593f493f6124c9609f63cc51395
Allan Mobley <allan.mobley.jr@qbcart.net>
2021-06-29 17:36:08 +0000

    remove bootstrap

--- dev/demo/index.tsx
+++ dev/demo/index.tsx
@@ -35,13 +35,6 @@ document.write(
       <link
         rel="stylesheet"
         data-preload="true"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-        crossorigin="anonymous"
-      />
-      <link
-        rel="stylesheet"
-        data-preload="true"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
       />
       <link
@@ -65,18 +58,6 @@ document.write(
         type="module"
         integrity="sha384-LH/mFhEGRB4jHedP0nqOoIUwc4VX8eWJxEL+qTGWtroqiLJ2vxX169J0oSBMHL5o"
         src="/_snowpack/hmr-error-overlay.js"
-      ></script>
-      <script
-        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"
-        defer
-      ></script>
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"
-        defer
       ></script>
       <script type="module" defer>
         import 'https://qbcdemo.z13.web.core.windows.net/eshop/app-shell/index.js';
